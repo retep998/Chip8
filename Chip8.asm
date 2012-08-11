7aa7599f4b346455916f520a61d8f93ebc0c7c39
retep998 <retep998@verizon.net>
2012-08-10 20:35:32 -0400

    More progress on the emulator

--- Chip8.asm
+++ Chip8.asm
@@ -1,36 +1,34 @@
 .data
 extern Memory:dq
-extern Counter:dq
+extern Vars:dq
+extern Pixels:db
 .code
 LogicLoop proc
     ; Variables
     mov rax, 0
     mov rbx, 0
     mov rcx, 0
-    mov rdx, 0 ; Sub Counter
+    mov rdx, 0
     mov rsi, offset [Memory + 200h] ; Instruction pointer
     mov rdi, offset [Memory] ; Memory pointer
     ; Constants
-    mov r8, offset [Counter] ; Counter
+    mov r8, offset [Vars] ; Various variables
     mov r9, offset [Memory] ; Base memory pointer
-    mov r10, offset [jumpt] ; Jump Table
-    mov r11, 0
+    mov r10, offset [jumpt] ; Jump table
+    mov r11, offset [Pixels] ; The screen
     mov r12, 0
     mov r13, 0
     mov r14, 0
     mov r15, 0
     jmp logloop
-    align 10h
-jumpt:
-    dq i0xxx, i1xxx, i2xxx, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno ; nxxx
-    dq unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, i00ex, unkno ; 00nx
-    align 10h
+    align 8h
 incc:
     inc qword ptr [r8]
     jmp logloop
-    align 10h
+    align 8h
 logloop:
-    inc dx
+    inc rcx
+    test cx, cx
     jz incc
     movzx rax, word ptr [rsi]
     add rsi, 2h
@@ -39,33 +37,75 @@ logloop:
     shr rbx, 0ch
     and rax, 0fffh
     jmp qword ptr [r10 + rbx * 8h]
-    align 10h
+    align 8h
 i0xxx:
+    test ah, ah
+    jnz unkno
     mov rbx, rax
     shr rbx, 4h
+    and rax, 0fh
     jmp qword ptr [r10 + rbx * 8h + 80h]
-    align 10h
+    align 8h
 i1xxx:
     lea rsi, qword ptr [r9 + rax]
     jmp logloop
-    align 10h
+    align 8h
 i2xxx:
     push rsi
     lea rsi, qword ptr [r9 + rax]
     jmp logloop
-    align 10h
+    align 8h
+i00cx:
+    jmp logloop ; Todo: Scroll x down
+    align 8h
 i00ex:
-    jmp logloop ; Todo: Jump to proper spots
-    align 10h
+    jmp qword ptr [r10 + rax * 8h + 100h]
+    align 8h
+i00fx:
+    jmp qword ptr [r10 + rax * 8h + 180h]
+    align 8h
 i00e0:
-    jmp logloop ; Todo: Clear screen
-    align 10h
+    mov rax, r11
+    mov rbx, 10h
+    mov rdx, 0h
+clearloop:
+    mov qword ptr [rax + 00h], rdx
+    mov qword ptr [rax + 08h], rdx
+    mov qword ptr [rax + 10h], rdx
+    mov qword ptr [rax + 18h], rdx
+    mov qword ptr [rax + 20h], rdx
+    mov qword ptr [rax + 28h], rdx
+    mov qword ptr [rax + 30h], rdx
+    mov qword ptr [rax + 38h], rdx
+    add rax, 40h
+    dec rbx
+    jnz clearloop
+    jmp logloop
+    align 8h
 i00ee:
     pop rsi
     jmp logloop
-    align 10h
+    align 8h
+i00fb:
+    jmp logloop ; Todo: Scroll 4 right
+    align 8h
+i00fc:
+    jmp logloop ; Todo: Scroll 4 left
+    align 8h
+i00fe:
+    jmp logloop ; Todo: Scroll 4 left
+    align 8h
+i00ff:
+    jmp logloop ; Todo: Scroll 4 left
+    align 8h
 unkno:
     int 3
     jmp logloop
+    align 8h
+jumpt:
+    dq i0xxx, i1xxx, i2xxx, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno ; inxxx
+    dq unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, i00cx, unkno, i00ex, unkno ; i00nx
+    dq i00e0, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, i00ee, unkno ; i00en
+    dq unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, unkno, i00fb, i00fc, unkno, unkno, unkno ; i00fn
 LogicLoop endp
 end
--- Main.cpp
+++ Main.cpp
@@ -15,123 +15,104 @@ namespace std {
 
 //Variables
 extern "C" {
-    void LogicLoop();
-    const size_t LowWidth = 0x40;
-    const size_t LowHeight = 0x20;
-    const size_t HighWidth = 0x80;
-    const size_t HighHeight = 0x40;
-    const size_t Zoom = 0x4;
-    size_t Width = LowWidth;
-    size_t Height = LowHeight;
-    size_t OldWidth = Width;
-    size_t OldHeight = Height;
-    uint8_t DT = 0;//Delay timer
-    uint8_t ST = 0;//Sound timer
-    size_t Counter;
-    bool Throttle = true;
-    SDL_Surface* Screen = nullptr;
-    uint8_t Pixels[HighHeight][HighWidth];
+    __declspec(noreturn) void LogicLoop();
+    uint64_t Pixels[0x40][0x2] = {0};
     uint8_t Memory[0x1000] = {0};
-    bool Keys[0x10];
-    uint8_t Font[0x50] = {
-        0xF0, 0x90, 0x90, 0x90, 0xF0,//0
-        0x20, 0x60, 0x20, 0x20, 0x70,//1
-        0xF0, 0x10, 0xF0, 0x80, 0xF0,//2
-        0xF0, 0x10, 0xF0, 0x10, 0xF0,//3
-        0x90, 0x90, 0xF0, 0x10, 0x10,//4
-        0xF0, 0x80, 0xF0, 0x10, 0xF0,//5
-        0xF0, 0x80, 0xF0, 0x90, 0xF0,//6
-        0xF0, 0x10, 0x20, 0x40, 0x40,//7
-        0xF0, 0x90, 0xF0, 0x90, 0xF0,//8
-        0xF0, 0x90, 0xF0, 0x10, 0xF0,//9
-        0xF0, 0x90, 0xF0, 0x90, 0x90,//A
-        0xE0, 0x90, 0xE0, 0x90, 0xE0,//B
-        0xF0, 0x80, 0x80, 0x80, 0xF0,//C
-        0xE0, 0x90, 0x90, 0x90, 0xE0,//D
-        0xF0, 0x80, 0xF0, 0x80, 0xF0,//E
-        0xF0, 0x80, 0xF0, 0x80, 0x80//F
-    };
+#pragma pack(1)
+    struct {
+        uint64_t Counter;
+        uint16_t Keys;
+        uint8_t Width;
+        uint8_t Height;
+        uint8_t DT;
+        uint8_t ST;
+        uint8_t Resized;
+    } Vars;
 }
-
-void ClearScreen() {
-    memset(Pixels, 0, sizeof(Pixels));
-}
-__declspec(noinline) bool DrawSprite(size_t x, size_t y, size_t s, uint8_t* d) {
-    bool inv;
-    size_t xx = x%Width;
-    size_t sub = xx&(sizeof(size_t)*8-1) + (sizeof(size_t)*8-8);
-    if (sub) {
-        size_t x1 = xx /= sizeof(size_t)*8;
-        size_t x2 = x1 + 1;
-        if (x2 == Width/sizeof(size_t)) x2 = 0;
-        for (; s > 0; --s, ++y, ++d) {
-            if (y == Height) y = 0;
-        }
-    } else {
-        for (; s > 0; --s, ++y, ++d) {
-            if (y == Height) y = 0;
-        }
-    }
-    return inv;
-}
+const size_t Zoom = 0x4;
+bool Throttle = true;
+SDL_Surface* Screen = nullptr;
+uint8_t Font[0x50] = {
+    0xF0, 0x90, 0x90, 0x90, 0xF0,//0
+    0x20, 0x60, 0x20, 0x20, 0x70,//1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0,//2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0,//3
+    0x90, 0x90, 0xF0, 0x10, 0x10,//4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0,//5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0,//6
+    0xF0, 0x10, 0x20, 0x40, 0x40,//7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0,//8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0,//9
+    0xF0, 0x90, 0xF0, 0x90, 0x90,//A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0,//B
+    0xF0, 0x80, 0x80, 0x80, 0xF0,//C
+    0xE0, 0x90, 0x90, 0x90, 0xE0,//D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0,//E
+    0xF0, 0x80, 0xF0, 0x80, 0x80//F
+};
 void HandleEvents() {
     SDL_Event e;
     while (SDL_PollEvent(&e)) {
-		switch(e.type) {
-		case SDL_QUIT:
+        switch(e.type) {
+        case SDL_QUIT:
             std::exit(0);
             break;
         case SDL_KEYDOWN:
             switch (e.key.keysym.sym) {
-            case SDLK_0: Keys[0x0] = true; break;
-            case SDLK_1: Keys[0x1] = true; break;
-            case SDLK_2: Keys[0x2] = true; break;
-            case SDLK_3: Keys[0x3] = true; break;
-            case SDLK_4: Keys[0x4] = true; break;
-            case SDLK_5: Keys[0x5] = true; break;
-            case SDLK_6: Keys[0x6] = true; break;
-            case SDLK_7: Keys[0x7] = true; break;
-            case SDLK_8: Keys[0x8] = true; break;
-            case SDLK_9: Keys[0x9] = true; break;
-            case SDLK_a: Keys[0xA] = true; break;
-            case SDLK_b: Keys[0xB] = true; break;
-            case SDLK_c: Keys[0xC] = true; break;
-            case SDLK_d: Keys[0xD] = true; break;
-            case SDLK_e: Keys[0xE] = true; break;
-            case SDLK_f: Keys[0xF] = true; break;
+            case SDLK_1: Vars.Keys |= 0x0001; break;
+            case SDLK_2: Vars.Keys |= 0x0002; break;
+            case SDLK_3: Vars.Keys |= 0x0004; break;
+            case SDLK_4: Vars.Keys |= 0x0008; break;
+            case SDLK_q: Vars.Keys |= 0x0010; break;
+            case SDLK_w: Vars.Keys |= 0x0020; break;
+            case SDLK_e: Vars.Keys |= 0x0040; break;
+            case SDLK_r: Vars.Keys |= 0x0080; break;
+            case SDLK_a: Vars.Keys |= 0x0100; break;
+            case SDLK_s: Vars.Keys |= 0x0200; break;
+            case SDLK_d: Vars.Keys |= 0x0400; break;
+            case SDLK_f: Vars.Keys |= 0x0800; break;
+            case SDLK_z: Vars.Keys |= 0x1000; break;
+            case SDLK_x: Vars.Keys |= 0x2000; break;
+            case SDLK_c: Vars.Keys |= 0x4000; break;
+            case SDLK_v: Vars.Keys |= 0x8000; break;
             case SDLK_t: Throttle = !Throttle; break;
+            case SDLK_ESCAPE: std::exit(0); break;
             }
             break;
         case SDL_KEYUP:
             switch (e.key.keysym.sym) {
-            case SDLK_0: Keys[0x0] = false; break;
-            case SDLK_1: Keys[0x1] = false; break;
-            case SDLK_2: Keys[0x2] = false; break;
-            case SDLK_3: Keys[0x3] = false; break;
-            case SDLK_4: Keys[0x4] = false; break;
-            case SDLK_5: Keys[0x5] = false; break;
-            case SDLK_6: Keys[0x6] = false; break;
-            case SDLK_7: Keys[0x7] = false; break;
-            case SDLK_8: Keys[0x8] = false; break;
-            case SDLK_9: Keys[0x9] = false; break;
-            case SDLK_a: Keys[0xA] = false; break;
-            case SDLK_b: Keys[0xB] = false; break;
-            case SDLK_c: Keys[0xC] = false; break;
-            case SDLK_d: Keys[0xD] = false; break;
-            case SDLK_e: Keys[0xE] = false; break;
-            case SDLK_f: Keys[0xF] = false; break;
+            case SDLK_1: Vars.Keys &= 0xfffe; break;
+            case SDLK_2: Vars.Keys &= 0xfffd; break;
+            case SDLK_3: Vars.Keys &= 0xfffb; break;
+            case SDLK_4: Vars.Keys &= 0xfff7; break;
+            case SDLK_q: Vars.Keys &= 0xffef; break;
+            case SDLK_w: Vars.Keys &= 0xffdf; break;
+            case SDLK_e: Vars.Keys &= 0xffbf; break;
+            case SDLK_r: Vars.Keys &= 0xff7f; break;
+            case SDLK_a: Vars.Keys &= 0xfeff; break;
+            case SDLK_s: Vars.Keys &= 0xfdff; break;
+            case SDLK_d: Vars.Keys &= 0xfbff; break;
+            case SDLK_f: Vars.Keys &= 0xf7ff; break;
+            case SDLK_z: Vars.Keys &= 0xefff; break;
+            case SDLK_x: Vars.Keys &= 0xdfff; break;
+            case SDLK_c: Vars.Keys &= 0xbfff; break;
+            case SDLK_v: Vars.Keys &= 0x7fff; break;
             }
             break;
         }
     }
 }
 void SetupScreen() {
-    Screen = SDL_SetVideoMode(Width*Zoom, Height*Zoom, 8, SDL_SWSURFACE);
+    Screen = SDL_SetVideoMode(Vars.Width*Zoom, Vars.Height*Zoom, 8, SDL_SWSURFACE);
     SDL_Color Colors[2] = {{0, 0, 0, 255}, {255, 255, 255, 255}};
     SDL_SetColors(Screen, Colors, 0, 2);
-    ClearScreen();
 }
 void Initialize() {
+    Vars.Keys = 0x0000;
+    Vars.Width = 0x40;
+    Vars.Height = 0x20;
+    Vars.DT = 0;
+    Vars.ST = 0;
     std::memcpy(Memory + 0x50, Font, 0x50);
     std::path p = "bench.ch8";
     size_t len = (size_t)std::file_size(p);
@@ -149,154 +130,35 @@ void MainLoop() {
     for (;;) {
         Time += std::chrono::milliseconds(Delay);
         std::this_thread::sleep_until(Time);
-        if (DT) --DT;
-        if (ST) {
-            --ST;
+        if (Vars.DT) --Vars.DT;
+        if (Vars.ST) {
+            --Vars.ST;
             Beep(5000, Delay/2);
         }
         HandleEvents();
-        if (OldWidth != Width || OldHeight != Height) {
-            OldWidth = Width, OldHeight = Height;
-            SetupScreen();
-        }
         {
-            size_t Cur = Counter;
+            size_t Cur = Vars.Counter;
             size_t Now = __rdtsc();
-            SDL_WM_SetCaption((std::to_string((Now - Last)/((Cur - Prev)*65536.)) + " CPL").c_str(), "");
+            double CPL = (Now - Last)/((Cur - Prev)*65536.);
+            std::string Str = std::to_string(CPL);
+            SDL_WM_SetCaption((Str.erase(Str.length()-5) + " Cycles per Instruction").c_str(), "");
             Prev = Cur;
             Last = Now;
         }
         SDL_Rect r = {0, 0, Zoom, Zoom};
-        for (size_t y = 0; y < Height; ++y) {
+        for (size_t y = 0; y < Vars.Height; ++y) {
             r.y = y*Zoom;
-            for (size_t x = 0; x < Width; ++x) {
+            for (size_t x = 0; x < Vars.Width; ++x) {
                 r.x = x*Zoom;
-                SDL_FillRect(Screen, &r, Pixels[y][x]);
+                SDL_FillRect(Screen, &r, _bittest64((int64_t*)&Pixels[y][x>>6], x&0x3f));
             }
         }
         SDL_Flip(Screen);
     }
 }
-
-//        switch (A) {
-//        case 0x0:
-//            switch (B) {
-//            case 0x0:
-//                switch (C) {
-//                case 0xE:
-//                    switch (D) {
-//                    case 0x0:
-//                        ClearScreen();
-//                        break;
-//                    case 0xE:
-//                        MP = *--SP;
-//                        break;
-//                    default: Unknown();
-//                    }
-//                    break;
-//                case 0xF:
-//                    switch (D) {
-//                    case 0xE:
-//                        Width = LowWidth;
-//                        Height = LowHeight;
-//                        break;
-//                    case 0xF:
-//                        Width = HighWidth;
-//                        Height = HighHeight;
-//                        break;
-//                    default: Unknown();
-//                    }
-//                    break;
-//                default: Unknown();
-//                }
-//                break;
-//            default: Unknown();
-//            }
-//            break;
-//        case 0x1:
-//            MP = &Memory[BCD];
-//            break;
-//        case 0x2:
-//            *SP++ = MP;
-//            MP = &Memory[BCD];
-//            break;
-//        case 0x3:
-//            if (V[B] == CD) MP += 2;
-//            break;
-//        case 0x4:
-//            if (V[B] != CD) MP += 2;
-//            break;
-//        case 0x5:
-//            if (V[B] == V[C]) MP += 2;
-//            break;
-//        case 0x6:
-//            V[B] = CD;
-//            break;
-//        case 0x7:
-//            V[B] += CD;
-//            break;
-//        case 0x8:
-//            switch (D) {
-//            case 0x0:
-//                V[B] = V[C];
-//                break;
-//            case 0x1:
-//                V[B] |= V[C];
-//                break;
-//            case 0x2:
-//                V[B] &= V[C];
-//                break;
-//            case 0x3:
-//                V[B] ^= V[C];
-//                break;
-//            case 0x5:
-//                V[0xF] = V[C]>V[B]?0:1;
-//                V[B] -= V[C];
-//                break;
-//            default: Unknown();
-//            }
-//            break;
-//        case 0x9:
-//            if (V[B] != V[C]) MP += 2;
-//            break;
-//        case 0xA:
-//            I = BCD;
-//            break;
-//        case 0xC:
-//            V[B] = rand()%((uint16_t)CD+1);
-//            break;
-//        case 0xD:
-//            V[0xF] = DrawSprite(V[B], V[C], D, &Memory[I]);
-//            break;
-//        case 0xF:
-//            switch (CD) {
-//            case 0x07:
-//                V[B] = DT;
-//                break;
-//            case 0x15:
-//                DT = V[B];
-//                break;
-//            case 0x18:
-//                ST = V[B];
-//                break;
-//            case 0x29:
-//                I = 0x50 + 5*V[B];
-//                break;
-//            case 0x30://Fuuuuuu
-//                break;
-//            default: Unknown();
-//            }
-//            break;
-//        default: Unknown();
-//        }
-//    }
-//}
-__declspec(noreturn) void ThreadFunc() {
-    LogicLoop();
-}
 void Chip8() {
     Initialize();
-    std::thread Thread(ThreadFunc);
+    std::thread Thread(LogicLoop);
     Thread.detach();
     MainLoop();
 }
